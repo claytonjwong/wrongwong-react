b47f6858400408399361f2f1cab99d11dd792625
Clayton Wong <clayton.wong@ookla.com>
2019-07-26 09:57:36 -0700

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# wrongwong-react
---
+++ precache-manifest.0e19f17b9c51a1ddffc10a321c5564a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "516a9eaf948b0ac5bc1a5f9eec55ce1c",
+    "url": "/wrongwong-react/index.html"
+  },
+  {
+    "revision": "73b22b4efc4f421623d7",
+    "url": "/wrongwong-react/static/css/2.794d5f5b.chunk.css"
+  },
+  {
+    "revision": "42d7efc2aa39130606e2",
+    "url": "/wrongwong-react/static/css/main.24c17004.chunk.css"
+  },
+  {
+    "revision": "73b22b4efc4f421623d7",
+    "url": "/wrongwong-react/static/js/2.b133783a.chunk.js"
+  },
+  {
+    "revision": "42d7efc2aa39130606e2",
+    "url": "/wrongwong-react/static/js/main.4ca7e881.chunk.js"
+  },
+  {
+    "revision": "e169ea30457f45e5d346",
+    "url": "/wrongwong-react/static/js/runtime~main.2b49a1ed.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/wrongwong-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/wrongwong-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/wrongwong-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/wrongwong-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/wrongwong-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
